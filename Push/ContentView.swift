f0fd3351c4cc814f5587f952e905b1c1de917ae9
Manav Khanvilkar <manavkhanvilkar@Manavs-MacBook-Pro-2.local>
2026-06-28 13:19:11 -0700

    Add base MapKit home screen

--- Push/ContentView.swift
+++ Push/ContentView.swift
@@ -6,17 +6,33 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var region = MapDefaults.region
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Map(coordinateRegion: $region)
+            .ignoresSafeArea()
     }
+}
+
+private enum MapDefaults {
+    static let region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: latitude,
+            longitude: longitude
+        ),
+        span: MKCoordinateSpan(
+            latitudeDelta: latitudeDelta,
+            longitudeDelta: longitudeDelta
+        )
+    )
+
+    private static let latitude = 37.7749
+    private static let longitude = -122.4194
+    private static let latitudeDelta = 0.08
+    private static let longitudeDelta = 0.08
 }
 
 struct ContentView_Previews: PreviewProvider {
